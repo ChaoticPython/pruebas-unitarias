d65a3a202602d3d49b7bc39182f7bc853a209bea
Julio Efrain Pacheco <efrain.pacheco@ibm.com>
2021-04-14 12:20:42 -0500

    Agregue el plugin de SONARQUBE al pom.xml y además hice unos cambios que el compilador me pedía

--- src/test/java/com/unit/testing/PruebasUnitariasApplicationTest.java
+++ src/test/java/com/unit/testing/PruebasUnitariasApplicationTest.java
@@ -9,5 +9,5 @@ class PruebasUnitariasApplicationTest {
 	@Test
 	void contextLoads() {
 	}
-	
+
 }
--- src/test/java/com/unit/testing/dao/OperacionesDaoTest.java
+++ src/test/java/com/unit/testing/dao/OperacionesDaoTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import com.unit.testing.dao.OperationsDao;
-
 public class OperacionesDaoTest {
 
 	OperationsDao operations = new OperationsDao();
