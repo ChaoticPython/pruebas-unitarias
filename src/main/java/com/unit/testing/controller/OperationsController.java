1cb1869c1c237fc35f76d9a55afb0609a16618c9
Julio Efrain Pacheco <efrain.pacheco@ibm.com>
2021-04-15 13:42:53 -0500

    Se acomodaron los imports y se añadieron los javadoc necesarios para la clase 'OperationsController'

--- src/main/java/com/unit/testing/controller/OperationsController.java
+++ src/main/java/com/unit/testing/controller/OperationsController.java
@@ -3,6 +3,7 @@ package com.unit.testing.controller;
 import com.unit.testing.model.OperationRequest;
 import com.unit.testing.model.ResultResponse;
 import com.unit.testing.service.IVerifyRequestService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +12,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This class is the Controller of the Microservice.
+ * 
+ * @author JulioPachecoSanginez
+ *
+ */
 @RestController
 @RequestMapping("${micro-operations.context.path}")
 public class OperationsController {
 
+  /**
+   * An instance of the principal Service.
+   */
   @Autowired
   private IVerifyRequestService verifyRequestService;
 
+  /**
+   * The method receive the request with the parameters needed for the response
+   * 
+   * @param request
+   * @return a response with the result of the operation requested
+   */
   @PostMapping("${micro-operations.paths.operations}")
   public ResponseEntity<ResultResponse> retrieveResult(@RequestBody OperationRequest request) {
     ResultResponse response = verifyRequestService.retrieveResultResponse(request);
