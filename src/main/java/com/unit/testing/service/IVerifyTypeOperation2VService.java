7bf5f41f4959d303686a225fc3367d3f177ed06c
Julio Efrain Pacheco <efrain.pacheco@ibm.com>
2021-04-15 18:42:22 -0500

    Se agrego el javadoc de las interfaces de nuestros services

--- src/main/java/com/unit/testing/service/IVerifyTypeOperation2VService.java
+++ src/main/java/com/unit/testing/service/IVerifyTypeOperation2VService.java
@@ -2,8 +2,23 @@ package com.unit.testing.service;
 
 import com.unit.testing.model.ResultResponse;
 
+/**
+ * This is the interface for our Service that receives two values as parameter and the option.
+ * 
+ * @author JulioPachecoSanginez
+ *
+ */
 public interface IVerifyTypeOperation2VService {
 
+  /**
+   * We're going to use this interface method in our service that needs the values
+   * and the option wanted.
+   * 
+   * @param val1 as number.
+   * @param val2 as number.
+   * @param option as the operation.
+   * @return a response with the result of the operation.
+   */
   ResultResponse verifyTypeOperation(Double val1, Double val2, String option);
 
 }
