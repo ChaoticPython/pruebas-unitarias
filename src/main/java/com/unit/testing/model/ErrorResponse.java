1e0014bd51e9bf723ee99aae40c7be0cf31a3c5d
Julio Efrain Pacheco <efrain.pacheco@ibm.com>
2021-04-15 18:16:03 -0500

    Se agregaron los javadoc de las clases de modelo

--- src/main/java/com/unit/testing/model/ErrorResponse.java
+++ src/main/java/com/unit/testing/model/ErrorResponse.java
@@ -3,10 +3,19 @@ package com.unit.testing.model;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * This is a model class for our custom exceptions.
+ * 
+ * @author JulioPachecoSanginez
+ *
+ */
 @Getter
 @Setter
 public class ErrorResponse {
 
+  /**
+   * Our attributes for get the type, code and details of the exception.
+   */
   private String type;
 
   private String code;
--- src/main/java/com/unit/testing/model/OperationRequest.java
+++ src/main/java/com/unit/testing/model/OperationRequest.java
@@ -3,10 +3,19 @@ package com.unit.testing.model;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * This is a model class for our request.
+ * 
+ * @author JulioPachecoSanginez
+ *
+ */
 @Getter
 @Setter
 public class OperationRequest {
 
+  /**
+   * Our attributes for get the values and the operation option for process the request.
+   */
   private Double val1;
 
   private String option;
