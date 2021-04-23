faf5a1d74a923df17baa30550fecda0684192d7d
ChaoticPython <jeps_icc@outlook.com>
2021-04-23 18:34:46 -0500

    Se modificaron y eliminaron algunas pruebas unitarias

--- src/test/java/com/unit/testing/service/impl/VerifyRequestServiceImplTest.java
+++ src/test/java/com/unit/testing/service/impl/VerifyRequestServiceImplTest.java
@@ -2,17 +2,16 @@ package com.unit.testing.service.impl;
 
 import static org.junit.Assert.assertNotNull;
 
+import com.unit.testing.dao.OperationOneValueDao;
+import com.unit.testing.dao.OperationsTwoValuesDao;
 import com.unit.testing.exception.BadRequestException;
 import com.unit.testing.model.OperationRequest;
 import com.unit.testing.model.ResultResponse;
-import com.unit.testing.service.IVerifyTypeOperation1VService;
-import com.unit.testing.service.IVerifyTypeOperation2VService;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 /**
@@ -25,26 +24,26 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class VerifyRequestServiceImplTest {
 
   /**
-   * An instance of the service that receive one value (number).
+   * An instance of the DAO that receive one value (number).
    */
   @Mock
-  private IVerifyTypeOperation1VService verifyOperation1Value;
+  private OperationOneValueDao operationOneValueDao;
 
   /**
-   * An instance of the service that receive two values (numbers).
+   * An instance of the DAO that receive two values (numbers).
    */
   @Mock
-  private IVerifyTypeOperation2VService verifyOperation2Value;
+  private OperationsTwoValuesDao operationsTwoValuesDao;
 
   /**
-   * An instance of our principal service.
+   * An instance of our service.
    */
   @InjectMocks
   private VerifyRequestServiceImpl verifyRequest;
 
   /**
-   * Given a request,
-   * When the request is empty,
+   * Given a request, 
+   * When the request is empty, 
    * Then throw a BadRequestException.
    */
   @Test(expected = BadRequestException.class)
@@ -58,8 +57,8 @@ public class VerifyRequestServiceImplTest {
   }
 
   /**
-   * Given a request,
-   * When the request has the two values (numbers) as null,
+   * Given a request, 
+   * When the request has the two values (numbers) as null, 
    * Then throw a BadRequestException.
    */
   @Test(expected = BadRequestException.class)
@@ -73,8 +72,8 @@ public class VerifyRequestServiceImplTest {
   }
 
   /**
-   * Given a request,
-   * When the request has the second value (number) and the option null,
+   * Given a request, 
+   * When the request has the second value (number) and the option null, 
    * Then throw a BadRequestException.
    */
   @Test(expected = BadRequestException.class)
@@ -88,8 +87,8 @@ public class VerifyRequestServiceImplTest {
   }
 
   /**
-   * Given a request
-   * When the request has the first value (number) and the option null,
+   * Given a request,
+   * When the request has the first value (number) and the option null, 
    * Then throw a BadRequestException.
    */
   @Test(expected = BadRequestException.class)
@@ -103,8 +102,8 @@ public class VerifyRequestServiceImplTest {
   }
 
   /**
-   * Given a request,
-   * When the request only hast the option null,
+   * Given a request, 
+   * When the request only has the option null, 
    * Then throw a BadRequestException.
    */
   @Test(expected = BadRequestException.class)
@@ -116,53 +115,11 @@ public class VerifyRequestServiceImplTest {
 
     verifyRequest.retrieveResultResponse(request);
   }
-
+  
   /**
-   * Given a request,
-   * When the request has only the second value (number) as null,
-   * Then redirect to other service and get the response.
-   */
-  @Test
-  public void retrieveResultResponseWithFirstValue() {
-    OperationRequest request = new OperationRequest();
-    request.setFirstValue(1.2192);
-    request.setSecondValue(null);
-    request.setOption("option");
-
-    ResultResponse previewResponse = new ResultResponse();
-    Mockito.when(verifyOperation1Value.verifyTypeOperation(request.getFirstValue(), request.getOption()))
-        .thenReturn(previewResponse);
-
-    ResultResponse response = verifyRequest.retrieveResultResponse(request);
-
-    assertNotNull(response);
-  }
-
-  /**
-   * Given a request,
-   * When the request has only the first value (number) as null,
-   * Then redirect to other service and get the response.
-   */
-  @Test
-  public void retrieveResultResponseWithSecondValue() {
-    OperationRequest request = new OperationRequest();
-    request.setFirstValue(null);
-    request.setSecondValue(31.21);
-    request.setOption("option");
-
-    ResultResponse previewResponse = new ResultResponse();
-    Mockito.when(verifyOperation1Value.verifyTypeOperation(request.getSecondValue(), request.getOption()))
-        .thenReturn(previewResponse);
-
-    ResultResponse response = verifyRequest.retrieveResultResponse(request);
-
-    assertNotNull(response);
-  }
-
-  /**
-   * Given a request,
-   * When the request has all the values (numbers and option),
-   * Then redirect to other service and get the response.
+   * Given a request, 
+   * When the request has all the values (numbers and option), 
+   * Then do the operation desired and return the response.
    */
   @Test
   public void retrieveResultResponseWithValues() {
@@ -171,9 +128,39 @@ public class VerifyRequestServiceImplTest {
     request.setSecondValue(31.21);
     request.setOption("option");
 
-    ResultResponse previewResponse = new ResultResponse();
-    Mockito.when(verifyOperation2Value.verifyTypeOperation(request.getFirstValue(), request.getSecondValue(),
-        request.getOption())).thenReturn(previewResponse);
+    ResultResponse response = verifyRequest.retrieveResultResponse(request);
+
+    assertNotNull(response);
+  }
+
+  /**
+   * Given a request, 
+   * When the request has only the first value (number) as null, 
+   * Then do the operation desired and return the response.
+   */
+  @Test
+  public void retrieveResultResponseWithSecondValue() {
+    OperationRequest request = new OperationRequest();
+    request.setFirstValue(null);
+    request.setSecondValue(31.21);
+    request.setOption("option");
+
+    ResultResponse response = verifyRequest.retrieveResultResponse(request);
+
+    assertNotNull(response);
+  }
+
+  /**
+   * Given a request, 
+   * When the request has only the second value (number) as null, 
+   * Then do the operation desired and return the response.
+   */
+  @Test
+  public void retrieveResultResponseWithFirstValue() {
+    OperationRequest request = new OperationRequest();
+    request.setFirstValue(1.2192);
+    request.setSecondValue(null);
+    request.setOption("option");
 
     ResultResponse response = verifyRequest.retrieveResultResponse(request);
 
