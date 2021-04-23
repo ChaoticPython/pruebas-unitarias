29ac3d3dddc5e8b6d90e9981635aabf5e3573615
ChaoticPython <jeps_icc@outlook.com>
2021-04-22 19:27:58 -0500

    Se crearon otras nuevas para evitar la gran cantidad de if-else añadidos. Posteriormente se eliminaran las clases (services 1-2 values)

--- src/main/java/com/unit/testing/service/impl/VerifyRequestServiceImpl.java
+++ src/main/java/com/unit/testing/service/impl/VerifyRequestServiceImpl.java
@@ -3,16 +3,16 @@ package com.unit.testing.service.impl;
 import com.unit.testing.exception.BadRequestException;
 import com.unit.testing.model.OperationRequest;
 import com.unit.testing.model.ResultResponse;
+import com.unit.testing.service.DoOperationOneValue;
+import com.unit.testing.service.DoOperationsTwoValues;
 import com.unit.testing.service.IVerifyRequestService;
-import com.unit.testing.service.IVerifyTypeOperation1VService;
-import com.unit.testing.service.IVerifyTypeOperation2VService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * This is our principal Service class.
- * In here we redirect to the best option of our received request.
+ * This is our principal Service class. In here we redirect to the best option of our received
+ * request.
  * 
  * @author JulioPachecoSanginez
  *
@@ -23,18 +23,22 @@ public class VerifyRequestServiceImpl implements IVerifyRequestService {
   /**
    * An instance of other service that needs one value (number) as parameter.
    */
+  // @Autowired
+  // private IVerifyTypeOperation1VService verifyTypeOperation1VService;
   @Autowired
-  private IVerifyTypeOperation1VService verifyTypeOperation1VService;
+  private DoOperationOneValue doOperationsOneValue;
 
   /**
    * An instance of other service that needs two values (numbers) as parameters.
    */
+  // @Autowired
+  // private IVerifyTypeOperation2VService verifyTypeOperation2VService;
   @Autowired
-  private IVerifyTypeOperation2VService verifyTypeOperation2VService;
+  private DoOperationsTwoValues doOperationsTwoValues;
 
   /**
-   * The method receive the request and, based on that, redirect to the best 
-   * option to do the operation.
+   * The method receive the request and, based on that, redirect to the best option to do the
+   * operation.
    * 
    * @param request as request.
    * @return a response with the result of the operation requested.
@@ -44,23 +48,24 @@ public class VerifyRequestServiceImpl implements IVerifyRequestService {
     // Aquí voy a recibir mi resultado
     ResultResponse response = new ResultResponse();
     // Comprobamos nuestra petición
-    if (request.getFirstValue() == null && request.getSecondValue() == null || request.getOption() == null) {
+    if (request.getFirstValue() == null && request.getSecondValue() == null
+        || request.getOption() == null) {
       // Tiramos un error por request invalido
       throw new BadRequestException("Hacen falta datos en la petición o está vacía");
     } else {
       // Aquí dirigimos a otro Service según la opción
       if (request.getFirstValue() != null && request.getSecondValue() != null) {
         // Dirigimos a Service que maneja operaciones con 2 valores
-        response = verifyTypeOperation2VService.verifyTypeOperation(request.getFirstValue(),
-            request.getSecondValue(), request.getOption());
+        response.setResultado(doOperationsTwoValues.doOperations(request.getOption(),
+            request.getFirstValue(), request.getSecondValue()));
       } else if (request.getFirstValue() != null && request.getSecondValue() == null) {
         // Dirigimos a Service que maneja operaciones con 1 valor
-        response = verifyTypeOperation1VService.verifyTypeOperation(request.getFirstValue(),
-            request.getOption());
+        response.setResultado(
+            doOperationsOneValue.doOperation(request.getOption(), request.getFirstValue()));
       } else if (request.getFirstValue() == null && request.getSecondValue() != null) {
         // Dirigimos a Service que maneja operaciones con 1 valor
-        response = verifyTypeOperation1VService.verifyTypeOperation(request.getSecondValue(),
-            request.getOption());
+        response.setResultado(
+            doOperationsOneValue.doOperation(request.getOption(), request.getSecondValue()));
       }
     }
     return response;
